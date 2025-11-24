2619dc7dbcbed58e616db8cd82dba84c1b2b4de4
Kunjamin-1 <narutothhokage7th@gmail.com>
2025-11-24 12:07:55 -0500

    fixed a minor error

--- FreeTune-backend/src/controllers/user.controller.js
+++ FreeTune-backend/src/controllers/user.controller.js
@@ -8,7 +8,7 @@ const generateAccessTokenRefreshToken = async (user) => {
     const accessToken = user.generateAccessToken()
     const refreshToken = user.generateRefreshToken()
     user.refreshToken = refreshToken
-    await user.save({ validateBeforeSave: true })
+    await user.save({ validateBeforeSave: false })
     return { accessToken, refreshToken }
 }
 
