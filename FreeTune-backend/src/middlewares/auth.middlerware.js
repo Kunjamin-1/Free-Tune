5c3ca80674a993fa20a30c46d935a65d2ce653cb
Kunjamin-1 <narutothhokage7th@gmail.com>
2026-05-13 00:32:53 +0530

    fix: wrap jwt.verify() in try/catch to handle token errors

--- FreeTune-backend/src/middlewares/auth.middlerware.js
+++ FreeTune-backend/src/middlewares/auth.middlerware.js
@@ -1,30 +1,42 @@
 import { User } from "../models/user.model.js";
 import ApiError from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const verifyJWT = asyncHandler(async (req,_,next) => {
+const verifyJWT = asyncHandler(async (req, _, next) => {
+  const token =
+    req?.cookies?.accessToken ||
+    req?.header("Authorization")?.replace("Bearer ", "");
+  console.log("cookies:", req.cookies);
+  console.log("auth header:", req.header("Authorization"));
 
-    const token = req?.cookies?.accessToken || req?.header("Authorization")?.replace("Bearer ","")
+  if (!token) {
+    throw new ApiError(401, "you are not authorized");
+  }
 
-    if(!token){
-        throw new ApiError(401,"you are not authorized")
-    }
+  let decodedJWT;
+  try {
+    decodedJWT = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  } catch (error) {
+    throw new ApiError(401, "Invalid token");
+  }
 
-    const decodedJWT =  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)  
+  console.log("hi");
 
-    if(!decodedJWT){
-        throw new ApiError(401,"your token is invalid")
-    }
+  if (!decodedJWT) {
+    throw new ApiError(401, "your token is invalid");
+  }
 
-    const user = await User.findById(decodedJWT._id).select("-password -refreshToken -avatarPublicId")
+  const user = await User.findById(decodedJWT._id).select(
+    "-password -refreshToken -avatarPublicId",
+  );
 
-    if(!user){
-        throw new ApiError(500, "Error occured while authorizing you")
-    }
+  if (!user) {
+    throw new ApiError(500, "Error occured while authorizing you");
+  }
 
-    req.user = user
-    next()
-})
- 
-export {verifyJWT}
+  req.user = user;
+  next();
+});
+
+export { verifyJWT };
